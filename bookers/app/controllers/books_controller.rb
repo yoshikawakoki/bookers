270dd7b7a09520a8334ed0c8aadba2e6e7b0cadd
yoshikawakoki <cooler970708@gmail.com>
2021-10-17 06:01:34 +0000

    update

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -22,6 +22,12 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+  end
+
   def destroy
   end
 
